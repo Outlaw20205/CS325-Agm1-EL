0a2facaf04aae373ddf55e19cca0768880237297
Outlaw20205 <155694658+Outlaw20205@users.noreply.github.com>
2024-09-08 15:10:58 -0500

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@ def get_number(prompt):
     number = input(prompt)
     return number
 
-numberToSquare = int(get_number('Please insert a num'))
+numberToSquare = int(get_number('Please insert a num: '))
 newNumber = pow(numberToSquare, 2)
 
 print('The square is %d' % (newNumber))
