59c3e0a97e09827f1fe81f01b7c7429ade1d76f1
Outlaw20205 <ezlawrence2014@gmail.com>
2024-09-08 15:18:52 -0500

    Added Program2 to new branch

---
+++ program2.py
@@ -0,0 +1,35 @@
+cardinals_roster = {
+    0 : "Masyn Winn - SS",
+    11 : "Victor Scott II - OF",
+    12 : "Erik Fedde - SP",
+    13 : "Matt Carpenter - DH",
+    15 : "Jose Fermin - IF",
+    18 : "Jordan Walker - OF",
+    21 : "Lars Nootbaar - OF",
+    26 : "Luken Baker - DH",
+    27 : "Andrew Kittredge - RHP",
+    28 : "Nolan Arenado - 3B",
+    32 : "Steven Matz - SP",
+    33 : "Brendan Donovan - UT",
+    39 : "Miles Mikolas - SP",
+    41 : "Alec Burleson - UT",
+    43 : "Pedro Pages - C",
+    44 : "Kyle Gibson - SP",
+    46 : "Paul Goldschmidt  - 1B",
+    47 : "John King - LHP",
+    48 : "Ivan Herrera - C",
+    52 : "Matthew Liberatore - LHP",
+    53 : "Andre Pallante - SP",
+    54 : "Sonny Gray - SP",
+    55 : "Riley O'Brien - RHP",
+    56 : "Ryan Helsey - RHP",
+    58 : "Chris Roycroft - RHP",
+    59 : "JoJo Romero - LHP",
+    63 : "Michael Siani - OF",
+    64 : "Ryan Fernandez - RHP"  
+}
+
+print('The STL Cardinals Roster as of Sept. 8 2024:')
+
+for key in cardinals_roster:
+    print(key, cardinals_roster[key])
